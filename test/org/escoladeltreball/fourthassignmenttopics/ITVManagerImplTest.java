391463fb1bb68815dbd78b13fcbeb5719b00a24a
Pep <jmendez1@xtec.cat>
2017-10-14 21:33:56 +0200

    Test finished and project ready for deployment

--- test/org/escoladeltreball/fourthassignmenttopics/ITVManagerImplTest.java
+++ test/org/escoladeltreball/fourthassignmenttopics/ITVManagerImplTest.java
@@ -5,10 +5,15 @@ package org.escoladeltreball.fourthassignmenttopics;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -16,6 +21,8 @@ import org.junit.Test;
  *
  */
 public class ITVManagerImplTest {
+
+	ITVManagerImpl manager;
 
 	/**
 	 * @throws java.lang.Exception
@@ -36,6 +43,7 @@ public class ITVManagerImplTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		manager = new ITVManagerImpl("itv");
 	}
 
 	/**
@@ -43,30 +51,44 @@ public class ITVManagerImplTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		manager = null;
 	}
 
 	/**
-	 * Test method for {@link org.escoladeltreball.fourthassignmenttopics.ITVManagerImpl#getNext()}.
+	 * Test method for
+	 * {@link org.escoladeltreball.fourthassignmenttopics.ITVManagerImpl#getNext()}.
+	 * 
+	 * TODO This method output might changes over time
+	 * 
+	 * 5333244,3444gff,16/10/2017 11:15:00,terrassa
 	 */
 	@Test
-	public final void testGetNext() {
-		fail("Not yet implemented"); // TODO
+	public final void testGetNext() throws Exception {
+		assertEquals(new ITV(5333244, "3444gff", LocalDateTime.parse("16/10/2017 11:15:00", DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss")), "terrassa"),
+				manager.getNext());
+		assertEquals(5333244, manager.getNext().getId());
 	}
 
 	/**
-	 * Test method for {@link org.escoladeltreball.fourthassignmenttopics.ITVManagerImpl#sort(java.util.Comparator)}.
+	 * Test method for
+	 * {@link org.escoladeltreball.fourthassignmenttopics.ITVManagerImpl#sort(java.util.Comparator)}.
 	 */
 	@Test
-	public final void testSort() {
-		fail("Not yet implemented"); // TODO
+	public final void testSort() throws Exception {
+		List<ITV> sortedByWhere = manager.sort((itv1, itv2) -> itv1.getWhere().compareTo(itv2.getWhere()));
+		assertTrue(sortedByWhere.get(0).getWhere().equals("badalona"));
+		assertTrue(sortedByWhere.get(sortedByWhere.size() - 1).getWhere().equals("terrassa"));
 	}
 
 	/**
-	 * Test method for {@link org.escoladeltreball.fourthassignmenttopics.ITVManagerImpl#from(java.lang.String)}.
+	 * Test method for
+	 * {@link org.escoladeltreball.fourthassignmenttopics.ITVManagerImpl#from(java.lang.String)}.
 	 */
 	@Test
-	public final void testFrom() {
-		fail("Not yet implemented"); // TODO
+	public final void testFrom() throws Exception {
+		assertEquals(2, manager.from("terrassa").size());
+		assertEquals(1, manager.from("barcelona").size());
+		assertEquals(0, manager.from("tarragona").size());
 	}
 
 }
